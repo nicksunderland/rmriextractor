9e509cc0393268d214d3b97cea917b6fc2f87900
nicksunderland <nicholas.p.sunderland@gmail.com>
2021-12-02 22:23:17 +0000

    .

--- R/extract_report_data.R
+++ R/extract_report_data.R
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples extract_report_data(data.frame(report1 = "hello world 23.0%", report2 = "hello world 54.0%"),
-#' data.frame(percentage = "hello world (\\d+\\.?\\d*)(\\w*)"))
+#' data.frame(percentage = "hello world (?<value>\\d+\\.?\\d*)(?<units>\\w*)"))
 #'
 extract_report_data <- function(reports, variable_regexs){
 
@@ -25,10 +25,31 @@ extract_report_data <- function(reports, variable_regexs){
 inner_func <- function(text , regex_list){        # Define this function to apply to each row (col1 = the report, col2 = the list of regex for a particular variable)
   value <- NA_real_                               # set to NA
   units <- NA_character_                          # set to NA
-  for(i in seq_along(regex_list)){                # Cycle through each regex provided for each variable e.g. it might be c("find my variable this way (\d+)", "find my variable another way (\d+)"")
-    match_obj <- stringr::str_match(text, regex_list[[i]]) # Match the regex in the text report
-    try(value <- as.numeric(match_obj[2]))        # try to convert the result to a number
-    try(units <- match_obj[3])                    # try to extract the units
+  for(pattern in regex_list){                # Cycle through each regex provided for each variable e.g. it might be c("find my variable this way (\d+)", "find my variable another way (\d+)"")
+    # match_obj <- stringr::str_match(text, pattern) # Match the regex in the text report
+
+    match_obj <- regexpr(pattern, text, perl=TRUE)
+    value_str <- substr(text, attr(match_obj, "capture.start" )[,"value"],
+                              attr(match_obj, "capture.start" )[,"value"] +
+                              attr(match_obj, "capture.length")[,"value"] - 1)
+
+    # If value not found "capture.start" will be 0 or -1
+    if(attr(match_obj, "capture.start")[,"value"] > 0){
+      try(value <- as.numeric(value_str))        # try to convert the result to a number
+    }
+
+    # Units don't have to be in the regular expression
+    tryCatch(
+      expr = {units_str <- substr(text, attr(match_obj, "capture.start" )[,"units"],
+                                        attr(match_obj, "capture.start" )[,"units"] +
+                                        attr(match_obj, "capture.length")[,"units"] - 1)
+              units <- as.character(units_str)},
+      error= function(e) {
+        # Choose a return value in case of error
+        return(NA)
+      }
+    )
+
     if(!is.na(value)) break                       # if successful at finding a numerical value exit the loop
   }
   return(value)
